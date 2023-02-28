2b409f30f4bcdc3f305a8abed7f8e3b06863a188
Sebastian Rogala (PGS Software) <srogala@pgs-soft.com>
2023-02-28 13:25:39 +0100

    Fix color scheme options for vuetifty

--- assets/plugins/vuetify.js
+++ assets/plugins/vuetify.js
@@ -2,16 +2,18 @@ import {createVuetify} from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
 const opts = {
-  theme: {
-    themes: {
-      light: {
-        primary: '#1565c0',
-        secondary: '#64b5f6',
-        accent: '#78002e',
-        error: '#d50000',
-      },
+    theme: {
+        themes: {
+            light: {
+                colors: {
+                    primary: '#1565c0',
+                    secondary: '#64b5f6',
+                    accent: '#78002e',
+                    error: '#d50000',
+                }
+            },
+        }
     }
-  }
 }
 
 export default createVuetify(opts)
